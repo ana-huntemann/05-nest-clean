adc16948131f2852733f9539b8353a3f2408f6df
navestelar <ana.carol.huntemann@gmail.com>
2026-03-02 22:05:19 -0300

    test: e2e tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,4 +1,7 @@
 import { faker } from '@faker-js/faker'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { Injectable } from '@nestjs/common'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
@@ -21,4 +24,21 @@ export function makeQuestion(
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
